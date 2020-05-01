1469d8b4503314194f939d9350a3ab189650125a
scb4377 <scblevins27@gmail.com>
2020-05-01 01:04:46 -0400

    api enabled

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// document.getElementById('next').addEventListener('click', getQuotes);
-// window.addEventListener('load', getQuotes);
-// async function getQuotes() {
-//   const response = await fetch('https://api.quotable.io/random');
-//   const data = await response.json();
-//   document.getElementById('quote-text').textContent = `${data.content}`;
-//   document.getElementById('author').textContent = `-${data.author}`;
-// }
+document.getElementById('next').addEventListener('click', getQuotes);
+window.addEventListener('load', getQuotes);
+async function getQuotes() {
+  const response = await fetch('https://api.quotable.io/random');
+  const data = await response.json();
+  document.getElementById('quote-text').textContent = `${data.content}`;
+  document.getElementById('author').textContent = `-${data.author}`;
+}
