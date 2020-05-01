60dfad478e3e59222279a70b97d0af42a34514f4
scb4377 <scblevins27@gmail.com>
2020-05-01 12:46:29 -0400

    Added Twitter Capabilities

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-document.getElementById('next').addEventListener('click', getQuotes);
+document.getElementById('new-quote').addEventListener('click', getQuotes);
 window.addEventListener('load', getQuotes);
-async function getQuotes() {
+
+async function getQuotes(quote, author) {
   const response = await fetch('https://api.quotable.io/random');
   const data = await response.json();
-  document.getElementById('quote-text').textContent = `${data.content}`;
+  document.getElementById('text').textContent = `${data.content}`;
   document.getElementById('author').textContent = `-${data.author}`;
+  twitter(data);
+  function twitter(data) {
+    document
+      .getElementById('tweet-quote')
+      .setAttribute(
+        'href',
+        `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${data.content}${data.author}`
+      );
+  }
 }
